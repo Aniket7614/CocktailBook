46f68450c5ffcc3be23b36b5d90ff422e19e58ee
aniket-valuelabs <aniketk561@gmail.com>
2025-04-13 18:23:35 +0530

    Structure the data model in Model class

--- CocktailBook/Model/Cocktail.swift
+++ CocktailBook/Model/Cocktail.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct Cocktail: Identifiable, Codable, Equatable {
+    let id: String
+    let name: String
+    let type: CocktailType
+    let shortDescription: String
+    let longDescription: String
+    let preparationMinutes: Int
+    let imageName: String
+    let ingredients: [String]
+    
+    var isFavorite: Bool = false
+    
+    enum CocktailType: String, Codable {
+        case alcoholic
+        case nonAlcoholic = "non-alcoholic"
+    }
+}
