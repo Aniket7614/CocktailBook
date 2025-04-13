789b75bcfca27984c81295f8092aa17e3e836389
aniket-valuelabs <aniketk561@gmail.com>
2025-04-13 18:24:22 +0530

    Created enum Cocktail filter for all three moctkails in view model

--- CocktailBook/ViewModel/CoctailDetailViewModel.swift
+++ CocktailBook/ViewModel/CoctailDetailViewModel.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+import Combine
+
+enum CocktailFilter: String, CaseIterable {
+    case all = "All Cocktails"
+    case alcoholic = "Alcoholic Cocktails"
+    case nonAlcoholic = "Non-Alcoholic Cocktails"
+}
