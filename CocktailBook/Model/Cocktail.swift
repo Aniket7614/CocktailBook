baff7944266b4d8e2b75fc4d3ee595716fe522a8
aniket-valuelabs <aniketk561@gmail.com>
2025-04-13 18:10:03 +0530

    Create Folder structure inside CocktailBook with new files

---
+++ CocktailBook/Model/Cocktail.swift
@@ -0,0 +1,8 @@
+//
+//  Cocktail.swift
+//  CocktailBook
+//
+//  Created by Aniket Kumar on 13/04/25.
+//
+
+import Foundation
---
+++ CocktailBook/ViewModel/CoctailDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CoctailDetailViewModel.swift
+//  CocktailBook
+//
+//  Created by Aniket Kumar on 13/04/25.
+//
+
+import Foundation
